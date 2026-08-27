e31fbe11cb02fc79f896cac8f44578481e0563e1
Vasil Giguashvili <68991478+venom-ss@users.noreply.github.com>
2026-08-27 16:39:51 +0400

    Prepare Hydraulic Toolbox for repository rename
    Remove hardcoded test paths and make GitHub Pages URLs derive from the repository name.

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,7 +1,8 @@
 export const prerender = true;
-const paths = ['/test/', '/test/about','/test/privacy','/test/disclaimer','/test/services','/test/reference/bsp-thread','/test/guides/hose-sizing','/test/guides/pump-identification','/test/calculators/cylinder-force','/test/calculators/pump-flow','/test/calculators/motor-rpm','/test/calculators/hydraulic-power','/test/calculators/motor-torque','/test/calculators/hose-velocity','/test/calculators/cylinder-speed','/test/calculators/pressure-converter','/test/calculators/flow-converter'];
+const routes = ['', 'about','privacy','disclaimer','services','reference/bsp-thread','guides/hose-sizing','guides/pump-identification','calculators/cylinder-force','calculators/pump-flow','calculators/motor-rpm','calculators/hydraulic-power','calculators/motor-torque','calculators/hose-velocity','calculators/cylinder-speed','calculators/pressure-converter','calculators/flow-converter'];
 export function GET({ site }: { site?: URL }) {
   const origin = site ?? new URL('https://venom-ss.github.io');
-  const urls = paths.map(p => `<url><loc>${new URL(p, origin)}</loc></url>`).join('');
+  const base = import.meta.env.BASE_URL;
+  const urls = routes.map(route => `<url><loc>${new URL(`${base}${route}`, origin)}</loc></url>`).join('');
   return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`, { headers: { 'Content-Type': 'application/xml' } });
 }
